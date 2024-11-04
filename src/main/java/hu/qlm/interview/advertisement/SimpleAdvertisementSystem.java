d280cff3641ba0f26970ef02dfbf5b97a0f3786c
Csaba Szabó <szabcsabest@gmail.com>
2024-11-04 19:37:20 +0100

    switching to comparator

--- src/main/java/hu/qlm/interview/advertisement/SimpleAdvertisementSystem.java
+++ src/main/java/hu/qlm/interview/advertisement/SimpleAdvertisementSystem.java
@@ -1,37 +1,35 @@
 package hu.qlm.interview.advertisement;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class SimpleAdvertisementSystem implements AdvertisementSystem {
     private final List<Advertisement> advertisements = new LinkedList<>();
-    private final Queue<AdvertisementContainer> adQueue = new PriorityQueue<>();
+    private final AdvertisementComparator adComparator = new AdvertisementComparator();
+    private final Queue<Advertisement> adQueue = new PriorityQueue<>(adComparator);
 
     @Override
     public void registerAdvertisement(Advertisement ad) {
         advertisements.add(ad);
-        adQueue.add(new AdvertisementContainer(ad, 0));
+        adQueue.add(ad);
     }
 
     @Override
     public void showNextAdvertisement(int dayIndex) {
-        AdvertisementContainer nextContainer = adQueue.poll();
-        if (nextContainer == null) {
+        Advertisement ad = adQueue.poll();
+        if (ad == null) {
             System.out.println("All registered advertisements reached their max appearance");
             return;
         }
-        Advertisement nextAd = nextContainer.getAdvertisement();
-        nextAd.showAdvertisement();
-        Integer dailyAppearances = nextAd.getAllAppearences().get(dayIndex);
+        ad.showAdvertisement();
+        Integer dailyAppearances = ad.getAllAppearences().get(dayIndex);
         if (dailyAppearances == null) {
             dailyAppearances = 0;
         }
         dailyAppearances += 1;
-        nextAd.getAllAppearences().put(dayIndex, dailyAppearances);
-        if (nextAd.lastAppearence(dayIndex, dayIndex) < nextAd.getMaxAppearance()) {
-            adQueue.add(new AdvertisementContainer(nextAd, dayIndex));
+        ad.getAllAppearences().put(dayIndex, dailyAppearances);
+        if (ad.lastAppearence(dayIndex, dayIndex) < ad.getMaxAppearance()) {
+            adComparator.updateDayIndex(dayIndex);
+            adQueue.add(ad);
         }
     }
 
@@ -40,30 +38,24 @@ public class SimpleAdvertisementSystem implements AdvertisementSystem {
         return List.copyOf(advertisements);
     }
 
-    private static final class AdvertisementContainer implements Comparable<AdvertisementContainer> {
-        private final Advertisement advertisement;
-        private final int dayIndex;
+    private static final class AdvertisementComparator implements Comparator<Advertisement> {
+        private int dayIndex = 0;
 
-        public AdvertisementContainer(Advertisement advertisement, int dayIndex) {
-            this.advertisement = advertisement;
+        public void updateDayIndex(int dayIndex) {
             this.dayIndex = dayIndex;
         }
 
-        public Advertisement getAdvertisement() {
-            return advertisement;
-        }
-
         @Override
-        public int compareTo(AdvertisementContainer other) {
-            double thisAppearances = this.advertisement.lastAppearence(dayIndex, dayIndex);
-            double otherAppearances = other.advertisement.lastAppearence(dayIndex, dayIndex);
-            if (otherAppearances == 0 && thisAppearances == 0) {
-                return Double.compare(other.advertisement.getWeight(), this.advertisement.getWeight());
-            } else if (otherAppearances == 0) {
+        public int compare(Advertisement ad1, Advertisement ad2) {
+            double ad1Appearances = ad1.lastAppearence(dayIndex, dayIndex);
+            double ad2Appearances = ad2.lastAppearence(dayIndex, dayIndex);
+            if (ad2Appearances == 0 && ad1Appearances == 0) {
+                return Double.compare(ad2.getWeight(), ad1.getWeight());
+            } else if (ad2Appearances == 0) {
                 return 1;
             }
-            double appearanceRatio = thisAppearances / otherAppearances;
-            double weightRatio = this.advertisement.getWeight() / other.advertisement.getWeight();
+            double appearanceRatio = ad1Appearances / ad2Appearances;
+            double weightRatio = ad1.getWeight() / ad2.getWeight();
             int compared = Double.compare(appearanceRatio, weightRatio);
             if (compared == 0) {
                 return 1;
